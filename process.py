2d97cc79dafbb4dddc0e5d55fd8d2d5262b14851
Qwqwww <jollot@yandex.ru>
2025-05-17 19:06:59 +0700

    Добавлены комментарии по доработкам

--- process.py
+++ process.py
@@ -1,3 +1,14 @@
-def predict(area):
-    cost = 500_000 * area - 300_000
+# TODO: функция, которая загружает модель
+def load_model():
+    # sklearn 
+    # нейронные сети
+    # min-max scaler и т.п.
+    pass
+
+# TODO: передавать все параметры
+def predict(params):
+    # TODO: добавить вызов моделей
+    model = load_model()
+    
+    cost = model.predict(params)
     return cost
